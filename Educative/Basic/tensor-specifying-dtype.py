8774601a53b1f4c966a478f0002144f78c35cd11
Joshua Greenfield <j.l.g.jobhunt@gmail.com>
2025-02-08 18:22:17 -0800

    Copied the corrected tensor-specifying-dtype.py from the CPU PyTorch Environment to the GPU PyTorch Environment.

--- Educative/Basic/tensor-specifying-dtype.py
+++ Educative/Basic/tensor-specifying-dtype.py
@@ -14,36 +14,36 @@ print("The dtype for c is {}".format(c.dtype))
 d = torch.tensor([True], dtype=torch.bool)
 print("The dtype for d is {}".format(d.dtype))
 
-e = torch.tensor([], dtype=torch.int8)
+e = torch.tensor([8], dtype=torch.int8)
 print("The dtype for e is {}".format(e.dtype))
 
-f = torch.tensor([], dtype=torch.int16)
+f = torch.tensor([16], dtype=torch.int16)
 print("The dtype for f is {}".format(f.dtype))
 
-g = torch.tensor([], dtype=torch.int32)
+g = torch.tensor([32], dtype=torch.int32)
 print("The dtype for g is {}".format(g.dtype))
 
-h = torch.tensor([], dtype=torch.int64)
+h = torch.tensor([64], dtype=torch.int64)
 print("The dtype for h is {}".format(h.dtype))
 
-i = torch.tensor([],dtype=torch.uint8)
+i = torch.tensor([8],dtype=torch.uint8)
 print("The dtype for i is {}".format(i.dtype))
 
-j = torch.tensor([], dtype=torch.uint16)
+j = torch.tensor([16], dtype=torch.uint16)
 print("The dtype for j is {}".format(j.dtype))
 
-k = torch.tensor([], dtype=torch.uint32)
+k = torch.tensor([32], dtype=torch.uint32)
 print("The dtype for k is {}".format(k.dtype))
 
-l = torch.tensor([], dtype=torch.uint64)
+l = torch.tensor([64], dtype=torch.uint64)
 print("The dtype for l is {}".format(l.dtype))
 
-m = torch.tensor([], dtype=torch.float16)
+m = torch.tensor([16], dtype=torch.float16)
 print("The dtype for m is {}".format(m.dtype))
 
-n = torch.tensor([], dtype=torch.float32)
+n = torch.tensor([32], dtype=torch.float32)
 print("The dtype for n is {}".format(n.dtype))
 
-o = torch.tensor([], dtype=torch.float64)
+o = torch.tensor([64], dtype=torch.float64)
 print("The dtype for o is {}".format(o.dtype))
 
